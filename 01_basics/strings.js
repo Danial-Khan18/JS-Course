376d0d2b708861fc655234457b5688542c49b3c7
Danial Hasan Khan <khandanial222@gmail.com>
2025-08-07 21:58:04 +0000

    Strings

---
+++ 01_basics/strings.js
@@ -0,0 +1,16 @@
+// Strings in JavaScript can be referred to a group or combination of alphabets, numbers, or symbols closed wtihin single or double quotes.
+// String is a type of data in programming language which can be stored easliy under any variable name.
+// Examples:
+
+let new_string="This is a string data-type";
+
+let name="Danial Hasan Khan";
+
+const roll_number="6769";
+let phone_number="56589272";
+
+// all these are examples of string data types a string can also include numbers as shown below:
+
+let number_as_string="87866";
+console.log(`The output of "number_as_string" is ${number_as_string} and it's data-type is ${typeof number_as_string}.`)
+
